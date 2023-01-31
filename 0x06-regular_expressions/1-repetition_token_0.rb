a20556a1b1c554813177f0cdcdfe91b918a99c05
Mcjayoap <mcjayoap>
2023-01-31 21:28:20 +0000

    test

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,16 +1,14 @@
 #!/usr/bin/env ruby
 
+
 def match_regexp(string)
-  if string.match(/School/)
-    puts "#{string}"
+  if string.match(/^\d{3}-\d{2}-\d{4}$/)
+    puts "The string '#{string}' matches the regular expression."
   else
     puts "The string '#{string}' does not match the regular expression."
   end
 end
 
-# Accept one argument from the command line
 input_string = ARGV[0]
-
-# Pass the argument to the matching method
 match_regexp(input_string)
 
